7fb449ceb430030cc04f95072d6ae9c9d2eaeaf6
Duke <duke.chen@gmail.com>
2023-11-12 15:42:12 +0800

    docs: Add annotations

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,5 @@ WORKDIR /var/pytest
 
 RUN pip install -r /var/pytest/requirements.txt
 
+# 使用「exit $PIPESTATUS」讓 Docker 以當前的 exit code 退出容器，這樣 Jenkins 才能抓到正確的 exit code
 CMD /bin/sh -c "python test.py; exit $PIPESTATUS"
--- test.py
+++ test.py
@@ -4,4 +4,5 @@ import sys
 if __name__ == "__main__":
     exit_code: int = pytest.main(args=['pytests'])  # 相當於執行 pytest pytests
     print(f"exit_code: {exit_code}")
+    # 要用 pytest 測試的 exit code 退出 python 腳本，不然每次退出碼都是 0（沒發生異常）
     sys.exit(exit_code)
