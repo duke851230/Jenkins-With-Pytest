b11b5224be6e6d6db6ecfaae3ce95c1bea541418
Duke <duke.chen@gmail.com>
2023-11-12 15:25:03 +0800

    test

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pytest
+import sys
 
 if __name__ == "__main__":
     exit_code: int = pytest.main(args=['pytests'])  # 相當於執行 pytest pytests
     print(f"exit_code: {exit_code}")
+    sys.exit(exit_code)
